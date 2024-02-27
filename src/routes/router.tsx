350f5dfd52183633a30835f063a070c4b9ff179c
patienceIneza <inezapatience2@gmail.com>
2024-02-27 18:35:58 +0200

    Login

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,12 +1,12 @@
-import { Routes, Route } from 'react-router-dom';
-import Login from '@/app/Auth/login';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Widget from '@/app/dashboard/Widget';
 import AdminLayout from '@/components/Admin/DefaultLayout';
+import Login from '@/app/Auth/login';
 
 export default function AppRoutes() {
     return (
         <Routes>
-              <Route path="/" element={<AdminLayout />} />
+            <Route path="/" element={<Navigate to="/login" />} />
             <Route path="login" element={<Login />} />
             <Route path="admin" element={<AdminLayout />}>
                 <Route index element={<Widget />} />
