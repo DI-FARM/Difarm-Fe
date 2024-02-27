d0f2619b3797cf797eed6a24a2db4c12ec55ea51
princoo <princeineza@gmail.com>
2024-02-27 14:46:47 +0200

    feat(report):
    - generate report
    - option to export report as a file
    - option to edit data in a report

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,15 +2,23 @@ import { Routes, Route } from 'react-router-dom';
 import Login from '@/app/Auth/login';
 import Widget from '@/app/dashboard/Widget';
 import AdminLayout from '@/components/Admin/DefaultLayout';
+import { Report } from "@/components/Report/Report";
+import Sidebar from "@/components/Admin/Sidebar";
+import Header from "@/components/Admin/Header";
 
 export default function AppRoutes() {
     return (
+        <>
+        {/* <Header />
+        <Sidebar /> */}
         <Routes>
-              <Route path="/" element={<AdminLayout />} />
+            <Route path="/" element={<AdminLayout />} />
+            <Route path="admin/reports" element={<Report />} />
             <Route path="login" element={<Login />} />
             <Route path="admin" element={<AdminLayout />}>
                 <Route index element={<Widget />} />
             </Route>
         </Routes>
+        </>
     );
 }
